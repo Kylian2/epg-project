25c3319f82989d6b6aba67393c5c77282ec76f13
Kylian Richard <kylian_richard@icloud.com>
2023-11-05 21:16:25 +0100

    first commit

---
+++ public/script/script.js
@@ -0,0 +1,52 @@
+const body = document.body;
+let cardsIndex = document.querySelectorAll(".menu-defilant__card-conteneur__card");
+//querySelectorAll car il faut un tableau pour utliser la méthode forEach
+//getElementsByClassName renvoie une collection HTML
+
+cardsIndex.forEach((card) => {
+    card.addEventListener("click", function () {
+        let bigCard = document.querySelector(".card-devellope");
+        bigCard.classList.remove("card-devellope");
+        bigCard.classList.add('card-petite');
+        console.log(bigCard);
+        this.classList.add("card-devellope");
+        this.classList.remove("card-petite");
+    });
+});
+
+const hamburgers = document.querySelectorAll('.hamburger');
+const menu = document.getElementById('hamburger-navigation');
+
+hamburgers.forEach((hamburger) => {
+    hamburger.addEventListener("click", function () {
+        menu.classList.toggle('inactif');
+        body.classList.toggle("stop");
+        console.log('click');
+    });
+});
+
+deplieBtn = document.querySelectorAll('.deplie-btn');
+replieBtn = document.querySelectorAll('.replie-btn');
+
+deplieBtn.forEach((btn) =>{
+    btn.addEventListener('click',function(){
+        let cardContainer = btn.closest('.formations-conteneur__card');
+        let moreInfo = cardContainer.querySelector('.formations-conteneur__card__more-informations');
+        this.classList.toggle('inactif');
+        moreInfo.classList.toggle('inactif');
+        console.log(moreInfo);
+        console.log('click');
+    });
+});
+
+replieBtn.forEach((btn) =>{
+    btn.addEventListener('click',function(){
+        let cardContainer = btn.closest('.formations-conteneur__card');
+        let moreInfo = cardContainer.querySelector('.formations-conteneur__card__more-informations');
+        moreInfo.classList.toggle('inactif');
+        let deplieBtnLocal = cardContainer.querySelector('.deplie-btn');
+        deplieBtnLocal.classList.toggle('inactif');
+        console.log(moreInfo);
+        console.log('click');
+    });
+});
