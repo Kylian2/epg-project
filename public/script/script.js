d1ebb84f0af3b2075e8ddb91c3f7b652ef7c6a84
Kylian Richard <kylian_richard@icloud.com>
2023-11-10 11:42:46 +0100

    adding js for adapting content in contact page

--- public/script/script.js
+++ public/script/script.js
@@ -50,3 +50,44 @@ replieBtn.forEach((btn) =>{
         console.log('click');
     });
 });
+
+//Code pour la page de contact, le composant qui permet de changer les coordonées
+
+var campusBtn = document.querySelectorAll('.map-conteneur__header li');
+
+var adresse = document.getElementById('adresse-conteneur');
+var tel = document.getElementById('tel-conteneur');
+var mail = document.getElementById('mail-conteneur');
+var responsable = document.getElementById('responsable-conteneur');
+
+const informationsPratiques = {
+    campus1 : {
+        adresse: "Rue des Ecoles 25048 Saint-Etienne",
+        tel : "+33 (0)4 76 93 00 00",
+        mail : "campus1@formation-pro.fr",
+        responsable : "Mme Marie Poulain"
+    },
+    campus2 : {
+        adresse: "Rue du Commerce 25048 Saint-Etienne",
+        tel : "+33 (0)6 76 20 11 00",
+        mail : "campus2@formation-pro.fr",
+        responsable : "Mr Jean Pierre Dupont"
+    },
+    campus3 : {
+        adresse: "Rue des Marché 25048 Saint-Etienne",
+        tel : "+33 (0)6 76 20 11 00",
+        mail : "campus3@formation-pro.fr",
+        responsable : "Mlle Sophie Martin"
+    }
+}
+campusBtn.forEach((btn) =>{
+    btn.addEventListener('click',function(){
+        var btnSelection = document.querySelector('.selectionne');
+        btnSelection.classList.toggle('selectionne');
+        this.classList.toggle('selectionne');
+        adresse.textContent = informationsPratiques[this.getAttribute('value')]["adresse"];
+        tel.textContent = informationsPratiques[this.getAttribute('value')]["tel"];
+        mail.textContent = informationsPratiques[this.getAttribute('value')]["mail"];
+        responsable.textContent = informationsPratiques[this.getAttribute('value')]["responsable"];
+    });
+});
